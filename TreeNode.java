997c090418fd88e5382d1a5500c629f32ebab8e6
jannchie <jannchie@gmail.com>
2017-10-12 22:38:57 +0800

    2017/10/12

---
+++ TreeNode.java
@@ -0,0 +1,26 @@
+package xin.jannchie;
+
+import java.util.Stack;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left, right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer.
+     */
+    public int maxDepth(TreeNode root) {
+        // write your code here
+        int depth = 1;
+        int maxDepth = 1;
+        return maxDepth;
+       // Stack stack = new Stack();
+
+    }
+}

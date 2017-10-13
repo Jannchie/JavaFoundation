5124cc706db7bafb52e817a0ad06e88432ab5756
jannchie <jannchie@gmail.com>
2017-10-14 00:03:16 +0800

    2017/10/14

--- TreeNode.java
+++ TreeNode.java
@@ -15,31 +15,13 @@ public class TreeNode {
      * @param root: The root of binary tree.
      * @return: An integer.
      */
+
     public int maxDepth(TreeNode root) {
-        // write your code here
-        if (root == null) {
+        // write your code (here)
+        if(root == null)
             return 0;
-        }
-        int depth = 1;
-        int maxDepth = 1;
-        Stack<TreeNode> treeNodeStack = new Stack<>();
-        treeNodeStack.push(root);
-        TreeNode temp = root;
-        while (temp != null || !treeNodeStack.empty()) {
-            while (temp != null) {
-                treeNodeStack.push(temp);
-                temp = temp.left;
-                depth++;
-                if (depth > maxDepth) {
-                    maxDepth = depth;
-                }
-            }
-            if (!treeNodeStack.empty()) {
-                temp = treeNodeStack.pop();
-                depth--;
-                temp = temp.right;
-            }
-        }
-        return  maxDepth;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return leftDepth > rightDepth ? (leftDepth + 1) : (rightDepth + 1);
     }
 }
